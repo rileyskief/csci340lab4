106e8db90e4556f65f12c7ebcdeb8359438fd39c
Riley Skief <skiefri@hendrix.edu>
2023-10-02 21:12:01 -0500

    pics

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-$(document).ready(function() {
-    $('#fact').click(function() {
-      $.ajax({
-        dataType: "json",
-        url: "https://cat-fact.herokuapp.com/facts",
-        success: function(results) {
-          console.log(results["text"]);
-        },
-        error: function(xhr,status,error) {
-          console.log(error);
-        }
-      });
+function fact() {
+    $(document).ready(function() {
+        $('#fact').click(function() {
+            $.ajax({
+                dataType: "json",
+                url: "https://cat-fact.herokuapp.com/facts",
+                success: function(results) {
+                console.log(results["text"]);
+                },
+                error: function(xhr,status,error) {
+                console.log(error);
+                }
+            });
+        });
     });
-  });
+}
+  
 
-  $(document).ready(function() {
-    $('#music').click(function() {
-      $.ajax({
-        dataType: "json",
-        url: "https://cat-fact.herokuapp.com/facts",
-        success: function(results) {
-          console.log(results["text"]);
-        },
-        error: function(xhr,status,error) {
-          console.log(error);
-        }
+function music() {
+    $(document).ready(function() {
+        $('#music').click(function() {
+            $.ajax({
+                dataType: "json",
+                url: "https://binaryjazz.us/wp-json/genrenator/v1/genre/",
+                success: function(results) {
+                console.log(results["text"]);
+                },
+                error: function(xhr,status,error) {
+                console.log(error);
+                }
+            });
+        });
       });
-    });
-  });
+  }
+  function both() {
+    $(document).ready(function() {
+        $('#both').click(function() {
+            music();
+            fact();
+        });
+      });
+  }
+
+
