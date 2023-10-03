4a9147990795da05466c69d2a5d17455eed18416
Riley Skief <skiefri@hendrix.edu>
2023-10-02 20:50:38 -0500

    button css name

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-    $('#fact-button').click(function() {
+    $('#fact').click(function() {
+      $.ajax({
+        dataType: "json",
+        url: "https://cat-fact.herokuapp.com/facts",
+        success: function(results) {
+          console.log(results["text"]);
+        },
+        error: function(xhr,status,error) {
+          console.log(error);
+        }
+      });
+    });
+  });
+
+  $(document).ready(function() {
+    $('#music').click(function() {
       $.ajax({
         dataType: "json",
         url: "https://cat-fact.herokuapp.com/facts",
