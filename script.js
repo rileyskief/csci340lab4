57cb78e30cf96b15fdaa3654e4cde11683d04a2c
Riley Skief <skiefri@hendrix.edu>
2023-10-02 20:29:18 -0500

    js for fact button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+$(document).ready(function() {
+    $('#fact-button').click(function() {
+      $.ajax({
+        dataType: "json",
+        url: "https://cat-fact.herokuapp.com/facts",
+        success: function(results) {
+          console.log(results["text"]);
+        },
+        error: function(xhr,status,error) {
+          console.log(error);
+        }
+      });
+    });
+  });
