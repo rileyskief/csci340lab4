977dd3f0edf18a6e25608c694aa19b86f9f5a34e
Riley Skief <skiefri@hendrix.edu>
2023-10-09 22:30:30 -0500

    js

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 
-    $(document).ready(function() {
-        $('#fact').click(function() {
-            $.ajax({
+    // $(document).ready(function() {
+    //     $('#fact').click(function() {
+    //         $.ajax({
+    //             dataType: "json",
+    //             url: "https://cat-fact.herokuapp.com/facts",
+    //             success: function(results) {
+    //                 console.log(results["text"], results["setup"], results["delivery"]);},
+                    
+
+    //         });
+    //     });
+    // });
+    
+    //     $('#music').click(function() {
+    //         $.ajax({
+    //             dataType: "json",
+    //             url: "https://binaryjazz.us/wp-json/genrenator/v1/genre/",
+    //             success: function(results) {
+    //                 console.log(results["text"], results["setup"], results["delivery"]);},
+                
+    //         });
+    //     });
+
+
+
+
+
+
+        $(document).ready(function() {
+            $('#clicker').click(function() {
+              $.ajax({
                 dataType: "json",
                 url: "https://cat-fact.herokuapp.com/facts",
                 success: function(results) {
-                    console.log(results["text"], results["setup"], results["delivery"]);},
-                    
-
-            });
-        });
-    });
-    
-        $('#music').click(function() {
-            $.ajax({
+                  console.log(results["text"]);   
+                },
+                error: function(xhr,status,error) {
+                  console.log(error);
+                }
+              });
+              $.ajax({
                 dataType: "json",
                 url: "https://binaryjazz.us/wp-json/genrenator/v1/genre/",
                 success: function(results) {
-                    console.log(results["text"], results["setup"], results["delivery"]);},
-                
-            });
-        });
+                  console.log(results["joke"], results["setup"], results["delivery"]);
+                  $('#quote').text(results["setup"] + " " + results["delivery"])
+                  $('#quote').text(results["joke"])
+                }
+              })
+            });  
+          });
       
