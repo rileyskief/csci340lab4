75b7525e2dae8ff98bf359ff6110e384dcb881f7
Riley Skief <skiefri@hendrix.edu>
2023-10-09 21:43:21 -0500

    button fix

--- script.js
+++ script.js
@@ -21,26 +21,4 @@
                 
             });
         });
-    
-        $('#both').click(function() {
-            $('#fact').click(function() {
-                $.ajax({
-                    dataType: "json",
-                    url: "https://cat-fact.herokuapp.com/facts",
-                    success: function(results) {
-                        console.log(results["text"], results["setup"], results["delivery"]);},
-                        
-                });
-            });
-       
-            $('#music').click(function() {
-                $.ajax({
-                    dataType: "json",
-                    url: "https://binaryjazz.us/wp-json/genrenator/v1/genre/",
-                    success: function(results) {
-                        console.log(results["text"]); },
-                        
-                });
-            });
-        });
       
