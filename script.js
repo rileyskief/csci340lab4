06ac8b1fa7a6e81f021f5cab410dc5bb73dbf20d
Riley Skief <skiefri@hendrix.edu>
2023-10-09 22:39:49 -0500

    js fix

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
 
-    // $(document).ready(function() {
-    //     $('#fact').click(function() {
-    //         $.ajax({
-    //             dataType: "json",
-    //             url: "https://cat-fact.herokuapp.com/facts",
-    //             success: function(results) {
-    //                 console.log(results["text"], results["setup"], results["delivery"]);},
-                    
-
-    //         });
-    //     });
-    // });
-    
-    //     $('#music').click(function() {
-    //         $.ajax({
-    //             dataType: "json",
-    //             url: "https://binaryjazz.us/wp-json/genrenator/v1/genre/",
-    //             success: function(results) {
-    //                 console.log(results["text"], results["setup"], results["delivery"]);},
-                
-    //         });
-    //     });
-
-
-
-
-
-
         $(document).ready(function() {
             $('#clicker').click(function() {
               $.ajax({
@@ -44,8 +16,8 @@
                 url: "https://binaryjazz.us/wp-json/genrenator/v1/genre/",
                 success: function(results) {
                   console.log(results["joke"], results["setup"], results["delivery"]);
-                  $('#quote').text(results["setup"] + " " + results["delivery"])
-                  $('#quote').text(results["joke"])
+                  $('#fact').text(results["setup"] + " " + results["delivery"])
+                  $('#fact').text(results["joke"])
                 }
               })
             });  
