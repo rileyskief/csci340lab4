9036e8870067e4c085ca1d53a730bdaeb76244c8
Riley Skief <skiefri@hendrix.edu>
2023-10-10 13:55:26 -0500

    css

--- script.js
+++ script.js
@@ -5,7 +5,8 @@
                 dataType: "json",
                 url: "https://cat-fact.herokuapp.com/facts",
                 success: function(results) {
-                  console.log(results["text"]);   
+                  console.log(results[0]["text"]);   
+                  $('#fact').text(results[0]["text"])
                 },
                 error: function(xhr,status,error) {
                   console.log(error);
